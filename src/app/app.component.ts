483e59b508031dd14af248482d9ab80b5f308a04
Oleg <olegvinokurov@hotmail.com>
2025-06-20 00:03:56 -0700

    navigation logic don't show user name and logout if not login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,34 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Ordering/Tracking';
+  isLoginPage = false;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router) {
+    // Listen to route changes to determine if we're on the login page
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      this.isLoginPage = event.url === '/login' || event.url === '/';
+    });
+
+    // Check initial route
+    this.isLoginPage = this.router.url === '/login' || this.router.url === '/';
+  }
+
   navigateToHome() {
     this.router.navigate(['/home']);
   }
